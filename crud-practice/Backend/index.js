84e8bef1c56f7d7ab74cdadbd38fe7067537a4ff
Kinza <kinzashaikh38@gmail.com>
2022-07-28 22:47:19 +0500

    uploading ejs project

---
+++ crud-practice/Backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const fupload = require('express-fileupload');
+const mongoose = require('mongoose');
+const path = require('path');
+const studentRouter = require('./routes/StudentsRoutes')
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(fupload());
+
+dotenv.config();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.set('view engine','ejs');
+
+const port = process.env.PORT || 8745;
+const db = process.env.DB_URL;
+
+mongoose.connect(db).then(res=>{
+    console.log("Connected to db")
+}).catch(err=>{
+    console.log(err)
+})
+
+app.use(studentRouter)
+
+app.listen(port,()=>{
+    console.log(`listening to ${port}`)
+})
